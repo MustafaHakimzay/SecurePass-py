d104c809e2fbb557bf9c22447b23b491e8c4ce53
Mustafa Hakimzay <189481025+MustafaHakimzay@users.noreply.github.com>
2026-02-15 18:29:06 +0400

    Added option for users to generate custom length passwords

--- main.py
+++ main.py
@@ -16,15 +16,30 @@ def show_menu():
     user_choice = int(user_choice)
 
     if user_choice == 1:
-        password = generate_password()
+        length = input('Enter the length for the password, or 0 to use default length (16): ')
+        if not length.isdigit():
+            print('That is not a valid integer. Using default length value')
+            password = generate_password()
+        else:
+            length = int(length)
+            if length == 0:
+                password = generate_password()
+            if length < 8:
+                print('Passwords shorter than 8 characters are very insecure. Using default length (16)')
+                password = generate_password()
+            else:
+                password = generate_password(length)
         print(f'\nGenerated password: {password}')
+
     elif user_choice == 2:
         password = input(f'Enter password to analsye: ')
         analysis = analyse_password(password)
         print_analysis(analysis)  
+
     elif user_choice == 3:
         print('Exited')
         return False
+    
     else:
         print('Not a valid option. Please enter only 1, 2 or 3.')  
 
